334bc5f32c87c5e067410d1324ed3c809fb63e59
JC <jctrilamanila@gmail.com>
2021-01-30 05:04:22 -0500

    Sidebar css created

---
+++ src/Chat.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Chat.css";
+
+function Chat() {
+    return (
+        <div className="chat">
+            <div className="chat__header">
+                <h4>
+                To: <span className="chat__name">Channel name</span>
+                </h4>
+                <strong>Details</strong>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/Sidebar.js
@@ -0,0 +1,36 @@
+import { Avatar, IconButton } from "@material-ui/core"
+import React from 'react'
+import "./Sidebar.css"
+import SidebarChat from "./SidebarChat";
+import SearchIcon from "@material-ui/icons/Search"
+import RateReviewOutlinedIcon from "@material-ui/icons/RateReviewOutlined"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar className="sidebar__avatar"/>
+                <div className="sidebar__input">
+                    <SearchIcon />
+                    <input placeholder="Search" />
+                </div>
+
+                <IconButton variant="outlined" className="sidebar__inputButton">
+                    <RateReviewOutlinedIcon />
+                </IconButton>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
