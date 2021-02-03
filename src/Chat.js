16beb052fb8a9341fd1ce496e02db2b3eac412be
JC <jctrilamanila@gmail.com>
2021-02-03 00:26:08 -0500

    Made messages responsive in real time

--- src/Chat.js
+++ src/Chat.js
@@ -1,13 +1,50 @@
 import { IconButton } from "@material-ui/core";
 import MicNoneIcon from "@material-ui/icons/MicNone";
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { useSelector } from "react-redux";
 import "./Chat.css";
+import { selectChatId, selectChatName } from "./features/chatSlice";
+import db from "./firebase";
+import Message from "./Message";
+import firebase from "firebase";
+import { selectUser } from "./features/userSlice";
+import FlipMove from "react-flip-move";
 
 function Chat() {
+    const user = useSelector(selectUser);
     const [input, setInput] = useState("");
+    const chatName = useSelector(selectChatName);
+    const chatId = useSelector(selectChatId);
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        if (chatId) {
+            db.collection("chats")
+                .doc(chatId)
+                .collection("messages")
+                .orderBy("timestamp", "desc")
+                .onSnapshot((snapshot) =>
+                    setMessages(
+                        snapshot.docs.map((doc) => ({
+                            id: doc.id,
+                            data: doc.data(),
+                        }))
+                    )
+                )
+        }
+    }, [chatId]);
 
     const sendMessage = (e) => {
         e.preventDefault();
+
+        db.collection("chats").doc(chatId).collection("messages").add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            uid: user.uid,
+            photo: user.photo,
+            email: user.email,
+            displayName: user.displayName,
+        });
 
         setInput("");
     };
@@ -17,17 +54,17 @@ function Chat() {
         <div className="chat">
             <div className="chat__header">
                 <h4>
-                To: <span className="chat__name">Channel name</span>
+                To: <span className="chat__name">{chatName}</span>
                 </h4>
                 <strong>Details</strong>
             </div>
 
-            <div className="chat__messsages">
-                <h2>I am a message</h2>
-                <h2>I am a message</h2>
-                <h2>I am a message</h2>
-                <h2>I am a message</h2>
-                <h2>I am a message</h2>
+            <div className="chat__messages">
+                <FlipMove>
+                    {messages.map(({ id, data }) => (
+                    <Message key={id} contents={data} />
+                    ))}
+                </FlipMove>
             </div>
 
             <div className="chat__input">
@@ -46,7 +83,7 @@ function Chat() {
                 </IconButton>
             </div>
         </div>
-    )
+    );
 }
 
 export default Chat
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,15 +1,46 @@
-import React from 'react'
-import { Avatar } from "@material-ui/core"
-import "./SidebarChat.css"
+import React from 'react';
+import { Avatar } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setChat } from "./features/chatSlice";
+import db from "./firebase";
+import "./SidebarChat.css";
+import * as timeago from 'timeago.js';
 
-function SidebarChat() {
+function SidebarChat({ id, chatName }) {
+        const dispatch = useDispatch();
+        const [chatInfo, setChatInfo] = useState([]);
+
+        useEffect(() =>{
+            db.collection("chats")
+                .doc(id)
+                .collection("messages")
+                .orderBy("timestamp", "desc")
+                .onSnapshot((snapshot) =>
+                    setChatInfo(snapshot.docs.map((doc) => doc.data()))
+            );
+        }, [id]);
+
     return (
-        <div className="sidebarChat">
-            <Avatar />
+        <div 
+            onClick={() =>
+                dispatch(
+                    setChat({
+                        chatId: id,
+                        chatName: chatName,
+                    })
+                )
+            } 
+            className="sidebarChat"
+        >
+            <Avatar src={chatInfo[0]?.photo} />
             <div className="sidebarChat__info">
-                <h3>Channel Name</h3>
-                <p>Last message sent...</p>
-                <small>timestamp</small>
+                <h3>{chatName}</h3>
+                <p>{chatInfo[0]?.message}</p>
+                <small>
+                    {timeago.format(new Date(chatInfo[0]?.timestamp?.toDate())
+                    )}
+                </small>
             </div>
         </div>
     )
